6cc996ca26fa5f74fcc7f7d4d7f15afc695a52a5
Dann239 <dann_pavlov@inbox.ru>
2019-10-13 05:04:10 +0300

    added upper bound inclusiveness guarantee for helper

--- scripts/helper.c
+++ scripts/helper.c
@@ -11,7 +11,9 @@ int main(int argc, char** argv) {
         args[0] = 1;
     if(argc <= 2)
         args[2] = args[1] = args[0];
-    for(double i = args[0]; i <= args[1]; i+= args[2]) {
+    if(args[2] <= 0)
+        return 0;
+    for(double i = args[0]; i < args[1] + 1e-6; i+= args[2]) {
         char cmd[512];
         sprintf(cmd, "\(echo \"variable scale string %.5lf\" && cat scripts/vars.Fe3O4 && cat scripts/in.Fe3O4) > /tmp/lmp_custom.Fe3O4 ; lmp -sf kk -k on t $(nproc) -i /tmp/lmp_custom.Fe3O4\n", i);
         system(cmd);
