3c97e1bacafd227d04c6d032f148eba54b13bd39
Dann239 <dann_pavlov@inbox.ru>
2019-10-14 04:22:50 +0000

    helper now uses masks to assign processes to cores

--- scripts/helper.c
+++ scripts/helper.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <sys/wait.h>
 #define min(a,b) (a < b ? a : b)
 
 int main(int argc, char** argv) {
-    int nproc;
-    char buff[2048];        
-    double args[3];
-    
+    int nproc, pind;
+    char mask[256];
+    char buff[2048];   
+    double args[3];     
+
     for(int i = 2; i < min(argc, 5); i++)
         sscanf(argv[i], "%lf", &args[i - 2]);
     switch(argc) {
@@ -25,16 +27,23 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-    int pind = 1;
-    for(; pind < nproc && fork(); pind++);
-    
+    if(args[2] <= 0 || nproc > 1024)
+        return 0;
+
+    for(pind = 1; pind < nproc && fork(); pind++);
+
+    memset(mask, '0', 256);
+    mask[1] = 'x';
+    mask[(nproc - 1) / 4 + 2 - (pind - 1) / 4] = '0' + (1 << ((pind - 1) % 4));
+    mask[(nproc - 1) / 4 + 3] = 0;
+
     for(int i = pind - 1; i < (args[1] - args[0] + 1e-6) / args[2]; i += nproc) {
         double scale = args[0] + i * args[2];
         
         sprintf(buff, "output/logs/log_%.5lf.lammps", scale);
         dup2(open(buff, O_WRONLY | O_CREAT, 0644), STDOUT_FILENO);
         
-        sprintf(buff, "(echo \"variable scale string %.5lf\" && cat scripts/in.Fe3O4) | lmp -e none", scale);
+        sprintf(cmd, "(echo \"variable scale string %.5lf\" && cat scripts/in.Fe3O4) > /scratch/pavlov/tmp/in_%d.Fe3O4 ; taskset %s lmp -i /scratch/pavlov/tmp/in_%d.Fe3O4\n", scale, pind, mask, pind);
         system(buff);
     }
 
