a05b5fed00ed8842d50d00b0c31b3aae238dbb23
Dann239 <dann_pavlov@inbox.ru>
2019-10-14 18:01:32 +0000

    configured for try2

--- scripts/helper.c
+++ scripts/helper.c
@@ -8,42 +8,41 @@
 #define min(a,b) (a < b ? a : b)
 
 int main(int argc, char** argv) {
-    int nproc, pind;
+    unsigned int nproc = 1, pind = 1, nsteps = 1;
     char mask[256];
     char buff[2048];
-    double args[3];
+    double start = 1, end = 1;
 
-    for(int i = 2; i < min(argc, 5); i++)
-        sscanf(argv[i], "%lf", &args[i - 2]);
     switch(argc) {
-    case 2:
-        args[0] = 1;
-    case 3:
-        args[2] = args[1] = args[0];
     case 5:
+        sscanf(argv[4], "%d", &nsteps);
+    case 4:
+        sscanf(argv[3], "%lf", &end);
+        sscanf(argv[2], "%lf", &start);
+    case 2:
         sscanf(argv[1], "%d", &nproc);
         break;
     default:
         return 0;
     }
-
-    if(args[2] <= 0 || nproc > 1024)
+    if(nproc > 1024)
         return 0;
 
-    for(pind = 1; pind < nproc && fork(); pind++);
+    for(; pind < nproc && fork(); pind++);
 
     memset(mask, '0', 256);
     mask[1] = 'x';
     mask[(nproc - 1) / 4 + 2 - (pind - 1) / 4] = '0' + (1 << ((pind - 1) % 4));
     mask[(nproc - 1) / 4 + 3] = 0;
 
-    for(int i = pind - 1; i < (args[1] - args[0] + 1e-6) / args[2]; i += nproc) {
-        double scale = args[0] + i * args[2];
+    for(int i = pind - 1; i < nsteps; i += nproc) {
+        double scale = start + i * (end - start);
 
-        sprintf(buff, "output/logs/log_%.5lf.lammps", scale);
+        sprintf(buff, "output/logs/log_%.6lf.lammps", scale);
         dup2(open(buff, O_WRONLY | O_CREAT, 0644), STDOUT_FILENO);
-
-        sprintf(buff, "(echo \"variable scale string %.5lf\" && cat scripts/in.Fe3O4) > /tmp/in_%d_%d.Fe3O4 ; taskset %s lmp -i /tmp/in_%d_%d.Fe3O4\n", scale, pind, (i - pind + 1) / nproc + 1, mask, pind, (i - pind + 1) / nproc + 1);
+	
+        sprintf(buff, "(echo \"variable scale string %.6lf\" && cat scripts/in.Fe3O4) > /tmp/in_%d_%d.Fe3O4 ; taskset %s lmp -i /tmp/in_%d_%d.Fe3O4\n", scale, pind, (i - pind + 1) / nproc + 1, mask, pind, (i - pind + 1) / nproc + 1);
+        printf(buff);
         system(buff);
     }
 
--- scripts/launch_simulation.sh
+++ scripts/launch_simulation.sh
@@ -1,2 +1,3 @@
 mkdir output output/logs output/dumps output/data output/restarts 2>/dev/null
+echo Args: $(nproc) $@
 gcc scripts/helper.c -o /tmp/helper_forker -std=c99 && time /tmp/helper_forker $(nproc) $@
