ec499291c2ebf64273146872e8ba14fefcf02393
Dann239 <dann_pavlov@inbox.ru>
2019-10-14 01:12:38 +0000

    helper now uses forks and launches concurrent serial simulations

--- scripts/helper.c
+++ scripts/helper.c
@@ -1,22 +1,44 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#define min(a,b) (a < b ? a : b)
 
 int main(int argc, char** argv) {
-    if(argc == 3 || argc > 4)
-        return 0;
+    int nproc;
+    char buff[2048];        
     double args[3];
-    for(int i = 1; i < argc; i++)
-        sscanf(argv[i], "%lf", &args[i-1]);
-    if(argc == 1)
+    
+    for(int i = 2; i < min(argc, 5); i++)
+        sscanf(argv[i], "%lf", &args[i - 2]);
+    switch(argc) {
+    case 2:
         args[0] = 1;
-    if(argc <= 2)
+    case 3:
         args[2] = args[1] = args[0];
-    if(args[2] <= 0)
+    case 5:
+        sscanf(argv[1], "%d", &nproc);
+        break;
+    default:
         return 0;
-    for(double i = args[0]; i < args[1] + 1e-6; i+= args[2]) {
-        char cmd[512];
-        sprintf(cmd, "\(echo \"variable scale string %.5lf\" && cat scripts/vars.Fe3O4 && cat scripts/in.Fe3O4) > /tmp/lmp_custom.Fe3O4 ; lmp -sf kk -k on t $(nproc) -i /tmp/lmp_custom.Fe3O4\n", i);
-        system(cmd);
     }
+
+    int pind = 1;
+    for(; pind < nproc && fork(); pind++);
+    
+    for(int i = pind - 1; i < (args[1] - args[0] + 1e-6) / args[2]; i += nproc) {
+        double scale = args[0] + i * args[2];
+        
+        sprintf(buff, "output/logs/log_%.5lf.lammps", scale);
+        dup2(open(buff, O_WRONLY | O_CREAT, 0644), STDOUT_FILENO);
+        
+        sprintf(buff, "(echo \"variable scale string %.5lf\" && cat scripts/in.Fe3O4) | lmp -e none", scale);
+        system(buff);
+    }
+
+    for(int i = 1; i < nproc && pind == nproc; i++)
+        wait(NULL);
     return 0;
 }
--- scripts/launch_simulation.sh
+++ scripts/launch_simulation.sh
@@ -1,1 +1,2 @@
-gcc scripts/helper.c -o /tmp/helper && /tmp/helper $@
+mkdir output output/logs output/dumps output/data 2>/dev/null
+gcc scripts/helper.c -o /tmp/helper_forker && time /tmp/helper_forker $@
