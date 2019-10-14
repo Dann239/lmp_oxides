2a6aa6dbb3e304e409707b36c9550e1ed1d72fd5
Dann239 <dann_pavlov@inbox.ru>
2019-10-14 06:51:25 +0000

    modification to do a drill run

--- scripts/launch_simulation.sh
+++ scripts/launch_simulation.sh
@@ -1,2 +1,2 @@
-mkdir output output/logs output/dumps output/data 2>/dev/null
-gcc scripts/helper.c -o /tmp/helper_forker && time /tmp/helper_forker $@
+mkdir output output/logs output/dumps output/data output/restarts 2>/dev/null
+gcc scripts/helper.c -o /tmp/helper_forker -std=c99 && time /tmp/helper_forker $(nproc) $@
