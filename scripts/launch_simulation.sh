ffe430fc966916644ff39f7c0502bd366d55691f
Dann239 <dann_pavlov@inbox.ru>
2019-10-20 11:04:59 +0000

    try3 set up

--- scripts/launch_simulation.sh
+++
@@ -1,4 +0,0 @@
-mkdir output output/logs output/dumps output/data output/restarts 2>/dev/null
-echo Args: $(nproc) $@
-gcc scripts/helper.c -o /tmp/lammps_helper -std=c99 && time /tmp/lammps_helper $(nproc) $@
-rm /tmp/lammps_helper
